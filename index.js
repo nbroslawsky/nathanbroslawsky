29ba1ad9323ddaae39da9098a6885493039eba48
Nathan Broslawsky <nbroslawsky@gmail.com>
2019-02-03 21:08:12 -0800

    Let's read the headers

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 8080
 
 app.use((req, res, next) => {
-	console.log('stuff', req.url)
+	console.log('stuff', req.headers)
 	next()
 })
 if(process.env.NODE_ENV == 'production') {
@@ -14,7 +14,7 @@ if(process.env.NODE_ENV == 'production') {
 app.set('view engine', 'pug')
 app.use('/files', express.static(path.join(__dirname, 'static')))
 app.get('/', (req, res) => res.render('index', {
-	protocol: req.protocol
+	protocol: req.originalUrl
 }))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
