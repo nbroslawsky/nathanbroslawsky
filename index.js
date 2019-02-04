04b53e37fccfdaf7f2d56d69188f82940872a17a
Nathan Broslawsky <nbroslawsky@gmail.com>
2019-02-03 21:12:39 -0800

    Added https detection based on headers coming from nginx

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const path = require('path')
 const app = express()
 const port = 8080
 
-app.use((req, res, next) => {
-	console.log('stuff', req.headers)
-	next()
-})
+// if we're running in production
 if(process.env.NODE_ENV == 'production') {
-
+	app.use((req, res, next) => {
+		if(req.headers['x-forwarded-proto'] == 'http') {
+			res.redirect('https://' + req.headers.host + req.url)
+		} else {
+			next()
+		}
+	})
 }
 
 app.set('view engine', 'pug')
