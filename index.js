4050d42ec626bcaf63e2a9ba85604969d75bf843
Nathan Broslawsky <nbroslawsky@gmail.com>
2019-02-03 21:16:57 -0800

    Removing the isProduction check

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const path = require('path')
 const app = express()
 const port = 8080
 
-// if we're running in production
-console.log('NODE_ENV', process.env.NODE_ENV)
-if(process.env.NODE_ENV == 'production') {
-	app.use((req, res, next) => {
-		console.log('x-forwarded-proto', req.headers['x-forwarded-proto'])
-		if(req.headers['x-forwarded-proto'] == 'http') {
-			res.redirect('https://' + req.headers.host + req.url)
-		} else {
-			next()
-		}
-	})
-}
+app.use((req, res, next) => {
+	console.log('x-forwarded-proto', req.headers['x-forwarded-proto'])
+	if(req.headers['x-forwarded-proto'] == 'http') {
+		res.redirect('https://' + req.headers.host + req.url)
+	} else {
+		next()
+	}
+})
 
 app.set('view engine', 'pug')
 app.use('/files', express.static(path.join(__dirname, 'static')))
