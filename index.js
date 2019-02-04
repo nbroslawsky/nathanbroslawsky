294cc797e1c959158e42645cb75ca3083a892f4f
Nathan Broslawsky <nbroslawsky@gmail.com>
2019-02-03 21:05:20 -0800

    Testing protocol detection

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const path = require('path')
 const app = express()
 const port = 8080
 
+app.use((req, res, next) => {
+	console.log('stuff', req.url)
+	next()
+})
+if(process.env.NODE_ENV == 'production') {
+
+}
+
 app.set('view engine', 'pug')
 app.use('/files', express.static(path.join(__dirname, 'static')))
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('index', {
+	protocol: req.protocol
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
