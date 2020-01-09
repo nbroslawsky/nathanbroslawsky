3cef97bb09751d16b3fb664888929c4715ff8c55
Nathan Broslawsky <nbroslawsky@gmail.com>
2020-01-09 11:02:08 -0800

    Debugging around www redirect

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.use((req, res, next) => {
 })
 
 app.use((req, res, next) => {
-  console.log(process.env.NODE_ENV, req.headers.host.substring(4))
-  if(process.env.NODE_ENV === 'production' && req.headers.host.substring(4) !== 'www.') {
+  console.log(process.env.NODE_ENV, req.headers.host.substring(0, 4))
+  if(process.env.NODE_ENV === 'production' && req.headers.host.substring(0, 4) !== 'www.') {
     // res.redirect('https://www.' + req.headers.host + req.url)
     console.log(req.headers.host.substring(4), 'https://www.' + req.headers.host + req.url)
     next()
